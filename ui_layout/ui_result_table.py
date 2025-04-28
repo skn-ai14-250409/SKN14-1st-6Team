954dc608e77d8214fa6c2ed3e0602505c089f997
yooyonghwan111 <yyh971001@gmail.com>
2025-04-28 10:47:38 +0900

    Delete ui_layout directory

--- ui_layout/ui_result_table.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def show_results(df):
-    st.subheader("리콜 상세 내역")
-
-    with st.expander("필터링된 리콜 데이터 보기"):
-        st.dataframe(df, use_container_width=True)
-
-    st.caption(f"총 {len(df)}건의 리콜 내역이 검색되었습니다。")
